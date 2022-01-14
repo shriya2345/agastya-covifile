737f0310115348669c0d32ef7c3bf2552953cc4d
shriya2345 <73680842+shriya2345@users.noreply.github.com>
2022-01-14 16:50:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask,request,jsonify
+# from data import data
+import csv
+
+app = Flask(__name__)
+
+#add code to open csv file and convert data to list
+with open('covid-variants.csv', encoding='utf8') as file:
+    data = csv.reader(file)
+    listData = list(data)
+    # print(listData)
+    covidVar = listData[1:]  #this is to pop header
+
+@app.route("/")
+def index():
+    return jsonify({
+        "data":covidVar,
+        "message":"Successful"
+    }),200
+
+@app.route("/variant")
+def variant():
+    name = request.srgs.get('variant')
+    v_data = next(item for item in data if item["variant"]== variant)
+    return jsonify({
+        "data":v_data,
+        "message":"Successful"
+    }),200
+
+if __name__ == "__main__":
+    app.run()
